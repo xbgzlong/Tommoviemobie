ff17d1c7fb4b794cc4f79aa0cdb931a84c9dfa5e
tom <935850064@qq.com>
2017-10-19 17:06:06 +0800

    初始版本

---
+++ app.js
@@ -0,0 +1,11 @@
+(function (angular) {
+    // "use strict";
+    // start your ride
+    var app = angular.module('main', [
+     // 路由规则，先引用，先匹配
+      'home',
+      'details',
+      'movie_list',
+      'auto-active'
+      ])
+})(angular);
